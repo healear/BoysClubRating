0116ce0197f4f339c2794adfc5b2987eb94dee01
healear <43036013+healear@users.noreply.github.com>
2022-05-06 15:03:47 +1000

    [Back] Исправить логику чтения фильмов из базы данных

--- php/controller.php
+++ php/controller.php
@@ -16,9 +16,9 @@
                 $film_array = array();
                 $query = "
                         SELECT film.Id, film.Name, film.Year, film.Country, film.Genre, film.Rating, actor.Name AS Actor, award.Name AS Award FROM film 
-                        LEFT JOIN award ON award.Id_film = film.id 
-                        JOIN actormid ON actormid.Id_film = film.Id 
-                        LEFT JOIN actor ON actormid.Id_actor = actor.Id "; // Query returns all posible combinations of actor+award from a certain moive (needs to be taken care of inside js script)
+                        LEFT JOIN Award ON award.Id_film = film.id 
+                        LEFT JOIN actormid ON actormid.Id_film = film.Id 
+                        LEFT JOIN Actor ON actormid.Id_actor = actor.Id "; // Query returns all posible combinations of actor+award from a certain moive (needs to be taken care of inside js script)
                 $result = $conn->query($query);
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
